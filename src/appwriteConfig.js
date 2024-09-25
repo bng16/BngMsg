44c2ca4d949399f042281e2ecc911191b67f5cd0
bng16 <biswajitnag161619@gmail.com>
2024-09-25 22:33:21 +0530

    datat fatched

---
+++ src/appwriteConfig.js
@@ -0,0 +1,14 @@
+import { Client, Databases } from "appwrite";
+
+export const PROJECT_ID='66f40a450039be727c5b'
+export const DATABASE_ID="66f41005001e2037b8b1";
+export const COLLECTION_ID_MESSAGES="66f4101300091cb2bb97";
+
+const client= new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("66f40a450039be727c5b");
+
+export const datanases = new Databases(client);
+export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,14 +1,39 @@
 import { useEffect, useState } from 'react';
-import service from '../appwrite/config'; 
+import { datanases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appwriteConfig';
 
 function Room() {
+
+  const [messages, setMessages] = useState([])
+  
   useEffect(() => {
-    const result = service.getMessages();
-    console.log('res :: ',result);
+    getMessages();
   },[])
 
+  const getMessages = async () => {
+    const response=await datanases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGES);
+    console.log('RESPONSE : ',response);
+    setMessages(response.documents);
+  }
+
   return (
-    <div>Room</div>
+    <div>
+      <div>
+        {messages.map(message => (
+          <div key={message.$id}>
+            <div>
+              <p>
+                {message.$createdAt}
+              </p>
+            </div>
+            <div>
+              <span>
+                {message.body} 
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
